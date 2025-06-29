8317c3c03271cfc9432b9469cafb2dc500b04418
kejbeg <kejbegofficial@gmail.com>
2025-06-29 21:17:45 +0200

    some changes

--- hyprland/.config/hypr/scripts/screenshotOcr.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# screenshot + OCR script
-
-FILE=$(mktemp --suffix=.png)
-grim -g "$(slurp)" "$FILE"
-TEXT=$(tesseract "$FILE" -)
-echo "$TEXT" | wl-copy
-notify-send "OCR Result" "$TEXT"
-
